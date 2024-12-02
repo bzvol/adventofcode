116694fff11a2f4d8b13f14d3ee06310bee007b4
Benjamin Zvolenszki <zvolenszki.benjamin@gmail.com>
2024-12-02 20:16:01 +0100

    day 2 solution

--- day2.py
+++ day2.py
@@ -31,27 +31,16 @@ print('Answer for part 1:', len(safe_reports))
 
 # Part 2
 
-def is_safe_report(report: list) -> bool:
-    increasing = True
-    unsafe_levels = 0
 
-    diffs = [report[i] - report[i - 1] for i in range(1, len(report))]
-    num_increasing = len([diff for diff in diffs if diff > 0])
-    num_decreasing = len([diff for diff in diffs if diff < 0])
-    if num_decreasing > num_increasing:
-        increasing = False
-
-    for i in range(len(report) - 1, 0, -1):
-        diff = report[i] - report[i - 1]
-        p = (diff >= 1 and diff <= 3) if increasing else (
-            diff <= -1 and diff >= -3)
-        if not p:
-            unsafe_levels += 1
-            report.pop(i - 1)
-
-    return unsafe_levels <= 1
+def is_safe_report2(report: list) -> bool:
+    for i in range(len(report)):
+        r = report.copy()
+        r.pop(i)
+        if is_safe_report(r):
+            return True
+    return False
 
 
-safe_reports = list(filter(is_safe_report, reports))
+safe_reports = list(filter(is_safe_report2, reports))
 
 print('Answer for part 2:', len(safe_reports))
