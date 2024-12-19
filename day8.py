669feca3316ef497c09c110ced49b12cacdd137c
Benjamin Zvolenszki <zvolenszki.benjamin@gmail.com>
2024-12-19 18:29:27 +0100

    day 8 part 2 solution

--- day8.py
+++ day8.py
@@ -3,7 +3,7 @@
 from util import *
 from collections import defaultdict
 
-# Part 1
+# Part 1 & 2
 
 with (open_input(8) as f):
     lines = f.readlines()
@@ -17,11 +17,14 @@ with (open_input(8) as f):
                 antennas[fr].append((j, i))
 
 result_set = set()
+result_set_2 = set()
 for pos_ls in antennas.values():
     np = len(pos_ls)
     for i in range(np - 1):
         for j in range(i + 1, np):
             (p1x, p1y), (p2x, p2y) = pos_ls[i], pos_ls[j]
+
+            # Part 1
             dx, dy = p2x - p1x, p2y - p1y
             an1x, an1y, an2x, an2y = p1x - dx, p1y - dy, p2x + dx, p2y + dy
             if 0 <= an1x < xlen and 0 <= an1y < ylen:
@@ -29,6 +32,16 @@ for pos_ls in antennas.values():
             if 0 <= an2x < xlen and 0 <= an2y < ylen:
                 result_set.add((an2x, an2y))
 
-result = len(result_set)
+            # Part 2
+            while 0 <= p1x < xlen and 0 <= p1y < ylen:
+                result_set_2.add((p1x, p1y))
+                p1x, p1y = p1x - dx, p1y - dy
+            while 0 <= p2x < xlen and 0 <= p2y < ylen:
+                result_set_2.add((p2x, p2y))
+                p2x, p2y = p2x + dx, p2y + dy
 
-result1(result)
+result1_ = len(result_set)
+result2_ = len(result_set_2)
+
+result1(result1_)
+result2(result2_)
